b83fc1465946ad4b3cd12625f6536a027906c256
haomiao33 <130969711+haomiao33@users.noreply.github.com>
2024-08-26 18:35:45 +0800

    Update MultiImagePickerPlusPlugin.kt
    fix

--- android/src/main/kotlin/com/tianxin/multi_image_picker_plus/MultiImagePickerPlusPlugin.kt
+++ android/src/main/kotlin/com/tianxin/multi_image_picker_plus/MultiImagePickerPlusPlugin.kt
@@ -221,34 +221,52 @@ class MultiImagePickerPlusPlugin : FlutterPlugin, MethodCallHandler, ActivityAwa
     }
 
     private fun getCorrectlyOrientedImage(context: Context, photoUri: Uri): Bitmap? {
-        var `is` = context.contentResolver.openInputStream(photoUri)
-        if (`is` == null) {
+        // 打开输入流并使用 BufferedInputStream 包装
+        val inputStream = context.contentResolver.openInputStream(photoUri)
+        if (inputStream == null) {
             return null
         }
-        val dbo = BitmapFactory.Options()
-        dbo.inScaled = false
-        dbo.inSampleSize = 1
-        dbo.inJustDecodeBounds = true
-        BitmapFactory.decodeStream(`is`, null, dbo)
-        `is`?.close()
+        
+        val bufferedInputStream = BufferedInputStream(inputStream)
+        bufferedInputStream.mark(bufferedInputStream.available())
+        
+        // 获取图片的尺寸信息
+        val dbo = BitmapFactory.Options().apply {
+            inScaled = false
+            inSampleSize = 1
+            inJustDecodeBounds = true
+        }
+        BitmapFactory.decodeStream(bufferedInputStream, null, dbo)
+        
+        // 重置流到开始位置
+        bufferedInputStream.reset()
+        
+        // 获取图片的正确方向
         val orientation: Int = getOrientation(context, photoUri)
-        var srcBitmap: Bitmap
-        `is` = context.contentResolver.openInputStream(photoUri)
-         if (`is` == null) {
-            return null
+        
+        // 设置解码选项
+        val decodingOptions = BitmapFactory.Options().apply {
+            inSampleSize = 1 // 如果图片较大，可以调整这个值来减小图片大小
         }
-        srcBitmap = BitmapFactory.decodeStream(`is`)
-        `is`?.close()
+        
+        // 解码图片
+        var srcBitmap = BitmapFactory.decodeStream(bufferedInputStream, null, decodingOptions)
+        bufferedInputStream.close()  // 关闭输入流
+        
+        // 旋转图片到正确的方向
         if (orientation > 0) {
-            val matrix = Matrix()
-            matrix.postRotate(orientation.toFloat())
+            val matrix = Matrix().apply {
+                postRotate(orientation.toFloat())
+            }
             srcBitmap = Bitmap.createBitmap(
                 srcBitmap, 0, 0, srcBitmap.width,
                 srcBitmap.height, matrix, true
             )
         }
+        
         return srcBitmap
     }
+
 
     private fun getOrientation(context: Context, photoUri: Uri): Int {
         var rotationDegrees = 0
